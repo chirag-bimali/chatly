ee35ebf80efb62caf69e5578cba9a70341e759d3
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-07-08 14:39:15 +0545

    Fix: ProfileContextMenu

--- Frontend/src/Components/UserProfileMenu.jsx
+++ Frontend/src/Components/UserProfileMenu.jsx
@@ -1,17 +1,45 @@
 import UserProfileMenuItem from "./UserProfileMenuItems";
 import SettingsIcon from "../assets/settings-icon.svg?react";
 import LogoutIcon from "../assets/logout-icon.svg?react";
+import { useEffect, useRef, useState } from "react";
 
 export default function UserProfileMenu({ contextMenu }) {
+  const menuRef = useRef(null);
+  const [width, setWindth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (menuRef.current) {
+      setWindth(menuRef.current.offsetWidth);
+      setHeight(menuRef.current.offsetHeight);
+    }
+  }, [contextMenu]);
   if (!contextMenu.visible) return null;
 
+  const windowWidth = window.innerWidth; // e.g. 1920 (pixels)
+  const windowHeight = window.innerHeight; // e.g. 1080 (pixels)
+  // If the menu goes past the right edge, move it left
+  let posX = contextMenu.x;
+  if (contextMenu.x + width > windowWidth) {
+    posX = posX - width;
+  }
+
+  // If the menu goes past the bottom edge, move it up
+  let posY = contextMenu.y;
+  if (posY + height > windowHeight) {
+    posY = posY - height;
+  }
+
+  console.log(contextMenu);
   return (
     <div
-      className="absolute bg-neutral-200 rounded shadow-md z-50"
+      className="fixed bg-neutral-200 rounded shadow-md z-50"
+      id="context-menu"
       style={{
-        right: contextMenu.x,
-        top: contextMenu.y,
+        left: posX,
+        top: posY,
       }}
+      ref={menuRef}
     >
       <UserProfileMenuItem
         icon={SettingsIcon}

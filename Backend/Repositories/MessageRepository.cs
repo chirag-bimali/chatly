e1d475616969f2fd82f3d499d7ddf78ce9142866
chirag <70309029+chirag486a@users.noreply.github.com>
2025-07-09 20:30:17 +0545

    Revert "Merge pull request #68 from chirag-bimali/backend"
    This reverts commit 9beae8222c9a61047a3d6544deb5ee3f054c592e, reversing
    changes made to fa3f10e2fc19560841a18ef2ecb9b1d18b1860f9.

--- Backend/Repositories/MessageRepository.cs
+++ Backend/Repositories/MessageRepository.cs
@@ -34,6 +34,11 @@ public class MessageRepository : IMessageRepository
         var contact = await _dbContext.Contacts.FirstOrDefaultAsync(x => x.Id == contactId);
         if (contact == null) throw new NotFoundException("Contact not found");
 
+        if (contact.Status != ContactStatus.Accepted)
+        {
+            throw new ConflictException("Contact is not accepted");
+        }
+
         var sender = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == senderId);
         if (sender == null) throw new NotFoundException("Sender not found");
 
@@ -80,8 +85,7 @@ public class MessageRepository : IMessageRepository
             Id = Guid.NewGuid().ToString(),
             ContactId = contact.Id,
             SenderId = sender.Id,
-            Content = content,
-            CreatedAt = DateTime.Now
+            Content = content
         };
         ReplyMessage? newReplyMessage = null;
         ForwardMessage? newforwardMessage = null;
@@ -144,15 +148,14 @@ public class MessageRepository : IMessageRepository
 
         var contact = await _dbContext.Contacts.FirstOrDefaultAsync(x => x.Id == contactId);
         if (contact == null) throw new NotFoundException("Contact not found");
+        if (contact.Status != ContactStatus.Accepted) throw new ConflictException("Contact is not accepted");
         if (!(contact.ContactId == userId || contact.UserId == userId))
             throw new ApplicationUnauthorizedAccessException("You are not authorized to access this contact");
         var count = await _dbContext.Messages.Where(x => x.ContactId == contactId).CountAsync();
         var queryable = _dbContext.Messages
             .Include(x => x.ForwardMessage)
             .Include(x => x.ReplyMessage)
-            .OrderByDescending(c => c.CreatedAt)
-            .Where(c => c.ContactId == contactId).Skip((page - 1) * pageSize).Take(pageSize)
-            .OrderBy(c => c.CreatedAt);
+            .Where(c => c.ContactId == contactId).Skip((page - 1) * pageSize).Take(pageSize);
         var replyMessages = await queryable.ToListAsync();
         return (await queryable.ToListAsync(), count);
     }

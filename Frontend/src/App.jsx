cf47eed7c700d295fd5c8d756c8c311c196f03c4
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-07-26 20:33:31 +0545

    Properly handle unauthentication

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,22 +15,39 @@ import ProtectedRoute from "./Components/ProtectedRoute";
 function App() {
   return (
     <AuthProvider>
-      <AppProvider>
-        <div className="bg-base max-h-dvh overflow-hidden h-dvh flex flex-col">
-          <Toaster position="bottom-right" />
-          <Router>
-            <Routes>
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="*" element={
+      <div className="bg-base max-h-dvh overflow-hidden h-dvh flex flex-col">
+        <Toaster position="bottom-right" />
+        <Router>
+          <Routes>
+            <Route
+              path="/signup"
+              element={
+                <AppProvider>
+                  <Signup />
+                </AppProvider>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <AppProvider>
+                  <Login />
+                </AppProvider>
+              }
+            />
+            <Route
+              path="*"
+              element={
                 <ProtectedRoute>
-                  <Home />
+                  <AppProvider>
+                    <Home />
+                  </AppProvider>
                 </ProtectedRoute>
-              } />
-            </Routes>
-          </Router>
-        </div>
-      </AppProvider>
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
     </AuthProvider>
   );
 }

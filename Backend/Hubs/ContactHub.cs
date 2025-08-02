e1c745dce5b4610fd99c43470df03ef1fcce322d
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-08-02 23:09:30 +0545

    Setup user active status

--- Backend/Hubs/ContactHub.cs
+++ Backend/Hubs/ContactHub.cs
@@ -1,4 +1,6 @@
+using Chatly.Data;
 using Chatly.DTO;
+using Chatly.Interfaces.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,4 +9,20 @@ namespace Chatly.Hubs;
 [Authorize]
 public class ContactHub : Hub
 {
+  private readonly IContactRepository _contactRepository;
+  public ContactHub(IContactRepository contactRepository)
+  {
+    _contactRepository = contactRepository;
+  }
+  public override Task OnConnectedAsync()
+  {
+    string connectionId = Context.ConnectionId;
+    string userId = Context.UserIdentifier ?? Context?.User?.Identity?.Name ?? connectionId;
+
+    // 1. Handle change status in db
+
+    // 2. Broadcast to all contact users
+
+    return base.OnConnectedAsync();
+  }
 }

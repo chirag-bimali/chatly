a366ff76d17e641ec6d6ff63e0b1ac1e3cffa1c9
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-08-07 22:44:06 +0545

    OnConnected Mark User Online

--- Backend/Hubs/UserHub.cs
+++ Backend/Hubs/UserHub.cs
@@ -1,4 +1,5 @@
 using Chatly.Interfaces.Repositories;
+using Chatly.Services;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Chatly.Hubs;
@@ -6,19 +7,22 @@ namespace Chatly.Hubs;
 public class UserHub : Hub
 {
     private IUserRepository _userRepository;
-    public UserHub(IUserRepository userRepository)
+    private PresenceTracker _presenceTracker;
+    public UserHub(IUserRepository userRepository, PresenceTracker presenceTracker)
     {
         _userRepository = userRepository;
+        _presenceTracker = presenceTracker;
     }
 
-    public async override Task OnConnectedAsync()
+    public override async Task OnConnectedAsync()
     {
         string connectionId = Context.ConnectionId;
         string userId = Context.UserIdentifier ?? Context?.User?.Identity?.Name ?? connectionId;
         
         
         // 1. Handle change status in db
-        await _userRepository.UpdateUserAsync(userId: userId, isOnline: true);
+        // await _userRepository.UpdateUserAsync(userId: userId, isOnline: true);
+        await _presenceTracker.MarkOnline(userId);
     
 
         // 2. Broadcast to all contact users

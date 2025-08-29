6b8482dee89a4f89158b6d70e7ca935306191234
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-08-29 16:05:38 +0545

    Setup for development and production environments

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import process from 'node:process'
 
 export default defineConfig({
   plugins: [tailwindcss(), react(), svgr()],
   server: {
     host: "0.0.0.0",
+    allowedHosts: ["chatly.chiragbimali.dev", "chatly.chiragbimali.com.np"], // 👈 add your custom domain here
     port: parseInt(process.env.VITE_PORT || "3000", 10),
     watch: {
       usePolling: true,
